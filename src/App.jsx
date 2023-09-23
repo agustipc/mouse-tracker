c55b1c2ce9b9a61b33e72914fbaf574a338c6297
Agustí Pou Company <agustipcom@gmail.com>
2023-09-23 20:21:29 +0200

    Create the mouse follower

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
-import './App.css'
+import { useEffect, useState } from 'react'
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // to clean up the event listener when the compoment unmounts and when the dependency changes
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 153, 255, 0.5)',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'DISABLE' : 'ENABLE'}
+      </button>
+    </>
+  )
+}
 
 function App () {
   return (
-    <h3>Proyect 3</h3>
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
